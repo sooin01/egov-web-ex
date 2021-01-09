eb7de273e3ab54b4ad9970f25782d6334626512b
sooin <sooin@Dongwook-Dell>
2021-01-09 20:49:45 +0900

    add

--- src/main/java/com/my/app/main/controller/MainController.java
+++ src/main/java/com/my/app/main/controller/MainController.java
@@ -1,5 +1,7 @@
 package com.my.app.main.controller;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,17 +11,28 @@ import com.my.app.main.service.MainService;
 
 @Controller
 public class MainController {
-	
+
 	@Autowired
 	private MainService mainService;
 
+	@PostConstruct
+	public void init() {
+		int mb = 1024 * 1024;
+
+		System.out.println("Total memory : " + Runtime.getRuntime().totalMemory() / mb + " MB");
+		System.out.println("Max memory : " + Runtime.getRuntime().maxMemory() / mb + " MB");
+		System.out.println("Free memory : " + Runtime.getRuntime().freeMemory() / mb + " MB");
+		System.out.println("Used memory : "
+				+ (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / mb + " MB");
+	}
+
 	@RequestMapping(value = "/main/index")
 	public ModelAndView index(ModelAndView mav) {
-		
+
 		mainService.getMain();
-		
+
 		mav.setViewName("main/index");
 		return mav;
 	}
-	
+
 }
